6aee058d57dd040e10a13f8f1c92e9723b69f19e
RashidatJay <rashidatjay@gmail.com>
2022-06-15 01:50:30 +0100

    Javascript task solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // complete the function
-function prime (num) {
-  // code goes here
+function prime(num) {
+  for (var i = 2; i < num; i++) if (num % i === 0) return false;
+  return num > 1;
 }
 
-function solution (arg) {
+function solution(arg) {
   return prime(arg);
 }
 
-if (typeof require !== "undefined" && require.main === module) {
+/*if (typeof require !== "undefined" && require.main === module) {
   if (process.argv.length <= 2) {
     throw Error("Require argument");
   }
@@ -16,4 +17,6 @@ if (typeof require !== "undefined" && require.main === module) {
   console.log(solution(arg));
 }
 
-module.exports = { solution };
+module.exports = { solution };*/
+let primeNumber = solution(44);
+console.log(primeNumber);
